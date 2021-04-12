43700e32f67796f5a72804272e1accaeff8d6372
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-04-12 15:03:34 +0200

    first test

---
+++ tests/ArticleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ArticlaTest extends TestCase
+{
+    public function testTitleIsEmptyByDefault()
+    {
+        $article = new App\Article;
+    }
+
+}
