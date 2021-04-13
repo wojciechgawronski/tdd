c2fa348fcdaf5f123e6282f3442472f7553906df
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-04-13 06:57:39 +0200

    refactor method to realize the test

--- src/App/Article.php
+++ src/App/Article.php
@@ -11,6 +11,8 @@ class Article
     {
         $slug = $this->title;
 
+        $slug = preg_replace('/[^\w]+/', ' ', $slug);
+
         $slug = preg_replace('/\s+/', '_', $slug);
 
         $slug = trim($slug, "_");
