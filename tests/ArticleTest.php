c968ce3daa567ec94d667a7f11f9b428c7a4887c
Wojciech Gawronski <wojciech@gawronsky.com.pl>
2021-04-13 07:22:14 +0200

    refactor tests method - make its clearer

--- tests/ArticleTest.php
+++ tests/ArticleTest.php
@@ -16,13 +16,16 @@ class ArticlaTest extends TestCase
         $this->assertEmpty($this->article->title);
     }
 
+    /**
+     * small php aspect reminder
+     */
     public function testSlugIsEmptyWithNoTitle()
     {
         // 1. null and empty string are equal..
         // 2. function foo() {}
         //    returns null..
         //    the same as: function foo() {return null;}
-        // php: compare null an empty string reteturn true
+        // php: compare null an empty string reteturns true
         
         // so, instead of:
         // $this->assertEquals($article->getSlug(), "");
@@ -30,39 +33,20 @@ class ArticlaTest extends TestCase
         $this->assertSame($this->article->getSlug(), "");
     }
 
-    /*
-    public function testSlugHasSpacesReplacedByUnderscores()
-    {
-        $this->article->title = "An example title";
-
-        $this->assertEquals($this->article->getSlug(), "An_example_title");
-    }
-
-    public function testSlugHashWithWhiteSpaces()
-    {
-        $this->article->title = "An      example \n title";
-
-        $this->assertEquals($this->article->getSlug(), "An_example_title");
-    }
-
-    public function testSlugDoesNotStartOrEndWithUnderscore()
-    {
-        $this->article->title = " read ≠ this! now %$# :-) ";
-
-        $this->assertEquals($this->article->getSlug(), "read_this_now");
-    }
-    */
-
     /**
      * https://phpunit.readthedocs.io/en/9.3/writing-tests-for-phpunit.html#data-providers
      */
     public function titleProvider()
     {
         return [
-            ["An example title", "An_example_title"],
-            [" An example title ", "An_example_title"],
-            ["An      example \n title", "An_example_title"],
-            [" read ≠ this! now %$# \r :-) ", "read_this_now"],
+            'slug spaces replaced by underscore' =>
+                    ["An example title", "An_example_title"],
+            'slug spaces should not have underscore at start and end position' =>
+                    [" An example title ", "An_example_title"],
+            'slug whitespaces replaced by underscore' =>
+                    ["An      example \n title", "An_example_title"],
+            'slug does not have non word characters' =>
+                    [" read ≠ this! now %$# \r :-) ", "read_this_now"],
         ];
     }
 
